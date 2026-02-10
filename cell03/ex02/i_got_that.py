683c54c3e96718b7e9aa1ee360f267b42c4e8d30
Natkawin Somroob <creeper0864@gmail.com>
2026-02-10 15:14:29 +0700

    Cellule3.2 - Python

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,6 @@
+user_input = str(input("What you gotta say? : "))
+while True:
+    if user_input == "STOP" :
+        break
+    else :
+        user_input = str(input("I got that! Anything else? : "))
