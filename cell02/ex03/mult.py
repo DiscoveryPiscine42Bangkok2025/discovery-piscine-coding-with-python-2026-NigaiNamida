9fe7af5193b695e77dda53be25243db595cf287a
Natkawin Somroob <creeper0864@gmail.com>
2026-02-10 14:58:44 +0700

    Cellule2.3 - Python

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,12 @@
+print("Enter the first number:")
+first_number = int(input("> "))
+print("Enter the second number:")
+second_number = int(input("> "))
+result = first_number * second_number
+print(f"{first_number} x {second_number} = {result}")
+if result > 0 :
+    print("The result is positive.")
+elif result < 0 :
+    print("The result is negative.")
+else :
+    print("The result is both positive and negative.")
