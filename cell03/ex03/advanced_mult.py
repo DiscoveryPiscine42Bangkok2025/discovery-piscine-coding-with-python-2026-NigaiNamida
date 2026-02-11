e04aef7620bd805e241aa1f6afe543d066c0dd7c
Natkawin Somroob <creeper0864@gmail.com>
2026-02-11 19:50:30 +0700

    Cellule3.3 - Python (fix)

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -3,7 +3,7 @@ while x <= 10 :
     print(f" Table de {x}: ", end="")
     y = 0 
     while y <= 10:
-        print(f"""{y * x}""", end=" ")
+        print(f"{y * x}", end=" ")
         y += 1
     print("")
     x += 1
